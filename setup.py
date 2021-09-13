c47d5565066d9ecc63b60a8978a777fd2c237bc3
Matt Eads <13305877+meads2@users.noreply.github.com>
2021-09-12 20:54:41 -0400

    updated buildplan

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name='saypy',
     author='Matt Eads',
-    version='0.0.1',
+    version='0.0.2',
     description='A very pointless, yet very friendly Python package.',
     long_description_content_type="text/markdown",
     packages=find_packages(),
